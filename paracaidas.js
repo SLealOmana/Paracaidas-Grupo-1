170dd547893d04edffb93b1c3b251521debde06b
私は朝食が好きです <56454387+SLealOmana@users.noreply.github.com>
2022-05-20 08:04:09 +0000

    Muchas cosas, cambio final

--- paracaidas.js
+++ paracaidas.js
@@ -20,12 +20,12 @@ function drawGrid(color, stepx, stepy) {
    }
    ctx.restore();
 }
-
-var canvas = document.getElementById('canvas_1'),
-ctx = canvas.getContext('2d');
+//Coloca recuadro blanco a la grafica
+var canvas = document.getElementById('canvas_1'),//Canvas o graficas
+ctx = canvas.getContext('2d');//Dimensiones de la grafica
 ctx.font = '13px Helvetica';
 var wChar= ctx.measureText('m').width;
-gravedad = 9.8;
+gravedad = 9.8;//valor de la gravedad inicial
 AlturaMaxima = parseFloat(document.getElementById('AlturaMaxima').value);
 var orgX=5*wChar, 
 orgY=canvas.height-2*wChar, 
@@ -42,7 +42,7 @@ orgXX=13*wChar,
 //peso del paracaidista
     m,
 //tiempo
-    dt=0.1, 
+    dt=parseFloat(document.getElementById('intervalos').value), 
 	t=0,
 //posición y velocidad
     x=0, 
@@ -51,8 +51,8 @@ orgXX=13*wChar,
 	x0, 
 	v0= 0, 
 	t0,
-//Altura maxima
- 
+//Densidad Aire
+  densidadAire = densidad(AlturaMaxima);
 //condiciones de caída
     caida=0,        //0 es libre, 1 con rozamiento, 2 en el límite
 	pol=[], //gráfica
@@ -127,7 +127,7 @@ function grafica(g){
 	}
 	g.stroke();
 }
-   
+//FUncion de la grafica 
   function mueveParticula(g){
     var y1=canvas.height-(AlturaMaxima-x)*escalaY;
     g.fillStyle='green';
@@ -146,7 +146,7 @@ function grafica(g){
         flechaFuerza(g, orgX+3*wChar, y1, 'blue', 1, k*v*v/m);
     }
   }
-  
+//Se encarga de mover la partuicula o paracaidas
  function flechaFuerza(g, x2, y2, color, sgn, f0){
 //longitud
     var lon=sgn*f0*2*wChar/gravedad;
@@ -169,11 +169,11 @@ function grafica(g){
 	g.closePath();
     g.fill();
 }
-
+//Es la del cuerpo libre
 function muestraValores(g){
 	g.textAlign='left';
 	g.textBaseline='top';
-    g.fillStyle='cyan';
+    g.fillStyle='lightBlue';
     g.fillRect(orgXX+8*wChar, 0, canvas.width-orgXX-4*wChar, 2*wChar);
     g.fillStyle='black';
 //tiempo
@@ -192,7 +192,7 @@ function muestraValores(g){
     x1+=(canvas.width-orgXX-10*wChar)/4;
     g.fillText('a: '+(-a).toFixed(1), x1, 2);
   }
-  
+//El recuadro azul claro que muestra los valores
  function dispositivo(g){
 //ejes
     grafica(g);
@@ -200,7 +200,7 @@ function muestraValores(g){
    mueveParticula(g);
    muestraValores(g);
   }
-
+//auxuliar
 	
 var raf, 
 	nuevo = document.getElementById('nuevo'),
@@ -231,16 +231,18 @@ nuevo.onclick = function (e) {
 	escalaYY=orgY/(AlturaMaxima/10), 
 	escalaY=(canvas.height-wChar)/AlturaMaxima;
 
-  
-  m=parseFloat(document.getElementById('masa_1').value);
-   	var area=parseFloat(document.getElementById('area_1').value);
+    dt=parseFloat(document.getElementById('intervalos').value),
+    m=parseFloat(document.getElementById('masa_1').value);
+   	area=parseFloat(document.getElementById('area_1').value);
     xInicial = 0;
-	  k=1.29*area*0.8/2;
+    coheficienteDeForma = parseFloat(document.getElementById('forma').value);
+    densidadAire = densidad(AlturaMaxima);   
+	  k=densidadAire*area*coheficienteDeForma/2;
     Vl=Math.sqrt(gravedad*m/k);
     t=0.0;
     V0 = 0;
     xMax = AlturaMaxima;
-    x=xMax-xInicial;         //2000 m
+    x=xMax-xInicial; 
     x_1 = x;  
     v=0;
     a=gravedad;
@@ -259,7 +261,7 @@ nuevo.onclick = function (e) {
         window.cancelAnimationFrame(raf);
     }
 }
-
+//Boton para abrir un nuevo ejemplo
 abre.onclick = function (e) {	
    abre.disabled=true;
    caida=1;
@@ -267,7 +269,7 @@ abre.onclick = function (e) {
     v0=gravedad*t;
     x0=gravedad*t*t/2;
 }
-
+//Boton para abrir el paracaidas
 empieza.onclick = function (e) {
   if(caida==0){
         abre.disabled=false;
@@ -278,7 +280,7 @@ empieza.onclick = function (e) {
 	pausa.style.display='inline';
 	raf=window.requestAnimationFrame(animate);
 }
-
+//boton para empezar el programa
 pausa.onclick = function (e) {
   empieza.disabled=false;
     pausa.disabled=true;
@@ -286,42 +288,49 @@ pausa.onclick = function (e) {
 	pausa.style.display='none';
     window.cancelAnimationFrame(raf);
 }
+//Boton para pausar el programa
 paso.onclick = function (e) {
     update();
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawGrid('lightgray', 10, 10);  
     dispositivo(ctx);
 }
-function update() {
+//boton para avanzar mas lentamente
+function update() {    
     switch(caida){
         case 0:        //libre antes de la apertura del paracaidsa
             a=gravedad;
             v=gravedad*t + V0;
             x=gravedad*t*t/2;
             break;
-        case 1:             //Al momento de abrir el paracaidas
+        case 1:        //Al momento de abrir el paracaidas
+            // Las formulas estan explicadas en el pdf
             var c=(v0+Vl)*Math.exp(Math.sqrt(gravedad*k/m)*(t-t0));
             var d=(v0-Vl)*Math.exp(-Math.sqrt(gravedad*k/m)*(t-t0));
-            v=Vl*(c+d)/(c-d) + V0;
-            a=gravedad-k*v*v/m;
-            x=x0-m*Math.log((Vl*Vl-v*v)/(Vl*Vl-v0*v0))/(2*k);
-            if(Math.abs(v-Vl)<0.001){
+            v=Vl*(c+d)/(c-d) + V0;//Velociad usando limite
+            a=gravedad-k*v*v/m;//Aceleracion
+            x=x0-m*Math.log((Vl*Vl-v*v)/(Vl*Vl-v0*v0))/(2*k);//Posicion
+            alturaActual = AlturaMaxima-x;//Altura actual usada para 
+            densidadAire = densidad(alturaActual); //Calculo de la densidad actual
+	          k=densidadAire*area*coheficienteDeForma/2;// Calculo de K en la pos Actual
+            
+            if(Math.abs(v-Vl)<0.000001){
                 caida=2;
                 x0=x;
                 v=Vl;
                 t0=t;
-            }
+            }//Cuando se alcanza la velocidad limite y se pasa de estar desacelrando a avanzar constante 
             break;
         case 2:   //movimiento uniforme
             a=0.0;
-            x=x0+Vl*(t-t0);
+            x=x0+Vl*(t-t0);//Formula velocidad terminal cuando ya no puede ir mas rapido
             break;
         default:
             break;
     }
-    t+=dt;  
+    t+=dt;//Actualizacion del tiempo en el intervalo establecido  
 }
-
+//Update actualiza los datos en el intervalo de tiempo establecido
 function animate(time) {
    update();
 	if (x>x_1){
@@ -334,3 +343,48 @@ function animate(time) {
     drawGrid('lightgray', 10, 10);  
     dispositivo(ctx);
 }
+//Animate es solo visual, anima respecto al tiempo 
+function densidad(altura)
+  {
+    densidadAire=0;
+    alturasPosibles = [0,100,200,500,1000,2000,3000,4000,5000,7500];
+    densidadesPosibles = [1.225,1.213,1.202,1.167,1.112,1.007,0.909,0.819,0.736,0.557];
+    if(altura>alturasPosibles[0]&&altura<=alturasPosibles[1])
+    {
+      densidadAire = densidadesPosibles[0];
+    }
+    if(altura>alturasPosibles[1]&&altura<=alturasPosibles[2])
+    {
+      densidadAire = densidadesPosibles[1];
+    }
+    if(altura>alturasPosibles[2]&&altura<=alturasPosibles[3])
+    {
+      densidadAire = densidadesPosibles[2];
+    }
+    if(altura>alturasPosibles[3]&&altura<=alturasPosibles[4])
+    {
+      densidadAire = densidadesPosibles[0];
+    }
+    if(altura>alturasPosibles[4]&&altura<=alturasPosibles[5])
+    {
+      densidadAire = densidadesPosibles[1];
+    }
+    if(altura>alturasPosibles[5]&&altura<=alturasPosibles[6])
+    {
+      densidadAire = densidadesPosibles[2];
+    }
+    if (altura<=alturasPosibles[7]&&altura>alturasPosibles[6])
+    {
+      densidadAire = densidadesPosibles[7];
+    }
+    if (altura<=alturasPosibles[8]&&altura>alturasPosibles[7])
+    {
+      densidadAire = densidadesPosibles[8];
+    } 
+    if (altura<=alturasPosibles[9]&&altura>alturasPosibles[8])
+    {
+      densidadAire = densidadesPosibles[9];
+    }
+    return densidadAire;
+  }
+//Funcion auxiliar creada para la variacion de densidad respecto a la altura
